4b202e95a4cf2d8806db301b714584c9879f793d
David Isidore <isidoredavid@protonmail.com>
2023-02-09 23:00:31 +0100

    Définition du style de la page 404

--- src/pages/NotFound/Notfound.js
+++ src/pages/NotFound/Notfound.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
+import './notFound.scss'
 
 export default function NotFound() {
     return(
-        <div>
+        <div className="notFound">
             <Header />
+            <div className="notFound__error">
+                <h1>404</h1>
+                <p>Oups! La page que vous demandez n'existe pas.</p>
+            </div>
             <Footer />
         </div>
     )
