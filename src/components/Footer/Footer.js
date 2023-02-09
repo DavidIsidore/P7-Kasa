6262efe1b74ac89c148cc84674d2f7cdb1ea6e94
David Isidore <isidoredavid@protonmail.com>
2023-02-10 00:14:31 +0100

    Définition du style du footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,12 @@
 import React from "react";
 import logo from '../../assets/logo.svg'
+import './footer.scss'
 
 export default function Footer() {
     return(
-        <div>
-            <img src={logo} alt="Kasa, location d'appartements" />
-            <p>© 2020 Kasa. All rights reserved</p>
+        <div className="footer">
+            <img src={logo} alt="Kasa, location d'appartements" className="footer__img"/>
+            <p className="footer__text">© 2020 Kasa. All rights reserved</p>
         </div>
     )
 }
