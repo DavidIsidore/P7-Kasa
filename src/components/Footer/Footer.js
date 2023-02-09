686480a51f1bfcb2bd0bac6a18415b8265846ed2
David Isidore <isidoredavid@protonmail.com>
2023-02-10 00:47:52 +0100

    Changement du logo du footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,11 @@
 import React from "react";
-import logo from '../../assets/logo.svg'
+import logoWhite from '../../assets/logo_white.svg'
 import './footer.scss'
 
 export default function Footer() {
     return(
         <div className="footer">
-            <img src={logo} alt="Kasa, location d'appartements" className="footer__img"/>
+            <img src={logoWhite} alt="Kasa, location d'appartements" className="footer__img"/>
             <p className="footer__text">© 2020 Kasa. All rights reserved</p>
         </div>
     )
