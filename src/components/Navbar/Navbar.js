e5325193c935a8ec833162672f2486ff868850cb
David Isidore <isidoredavid@protonmail.com>
2023-02-10 00:03:56 +0100

    Définition du style du header

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './navbar.scss'
 
 export default function Navbar() {
     return(
-        <nav>
-            <ul>
+        <nav className="nav">
+            <ul className="nav__list">
                 <li><Link to="/">Accueil</Link></li>
                 <li><Link to="/about">À propos</Link> </li>
             </ul>
