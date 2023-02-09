22c4180fad15b1a9722d02cd744378c9d3cc8804
David Isidore <isidoredavid@protonmail.com>
2023-02-09 21:46:50 +0100

    Création du footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from '../../assets/logo.svg'
+
+export default function Footer() {
+    return(
+        <div>
+            <img src={logo} alt="Kasa, location d'appartements" />
+            <p>© 2020 Kasa. All rights reserved</p>
+        </div>
+    )
+}
