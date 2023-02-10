05f94a3d34a154aefe6e8a6d1673cef7e7a491ef
David Isidore <isidoredavid@protonmail.com>
2023-02-10 13:21:25 +0100

    Ajout du style pour les Card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
+import './card.scss'
 
 export default function Card({id, cover, title}) {
     const idParam = useParams('id')
     console.log(idParam)
     return(
-        <Link to={`/accomodation/${id}`}>
+        <Link to={`/accomodation/${id}`} className="singleCard" >
             <img src={cover} alt={title} />
             <h3>{title}</h3>
         </Link>
