7c6463447d6e60a8fc7e8e9a556de102546d4ddd
David Isidore <isidoredavid@protonmail.com>
2023-02-10 09:28:36 +0100

    Création du composant Banner

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,8 @@
+import React from "react";
+import banner from '../../assets'
+
+export default function Banner() {
+    return(
+        <img src={banner} alt="bannière de la page d'accueil" />
+    )
+}
