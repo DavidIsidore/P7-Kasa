c22f42ea3a55bbeee19b482cfdc4d6e3998ab1bb
David Isidore <isidoredavid@protonmail.com>
2023-02-10 13:09:54 +0100

    Implémentation du Header dans la page accomodation

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Routes, Route} from 'react-router-dom'
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import NotFound from './pages/NotFound/Notfound';
+import Card from './components/Card/Card';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/accomodation/:id" element={<Card />} />
         <Route path="/*" element={<NotFound />} />
       </Routes>
     </div>
--- src/pages/Accomodation/Accomodation.js
+++ src/pages/Accomodation/Accomodation.js
@@ -1,7 +1,10 @@
 import React from "react";
+import Header from "../../components/Header/Header";
 
 export default function Accomodation() {
     return(
-        <div></div>
+        <div>
+            <Header />
+        </div>
     )
 }
