6e7b05a92b7caae70c19340afbccde65317e61f2
David Isidore <isidoredavid@protonmail.com>
2023-02-10 09:40:08 +0100

    Utilisation de useLocation pour afficher la bannière en fonction de la page

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,8 +1,17 @@
 import React from "react";
 import banner from '../../assets/home_banner.svg'
+import aboutBanner from '../../assets/about_banner.svg'
+import { useLocation } from "react-router-dom";
+
 
 export default function Banner() {
+    const location = useLocation()
+    console.log(location)
+    console.log(location.pathname)
     return(
-        <img src={banner} alt="bannière de la page d'accueil" />
+        location.pathname === "/about" ? (
+            <img src={aboutBanner} alt="Bannière de la page à propos" />
+        ):(
+        <img src={banner} alt="bannière de la page d'accueil" />)
     )
 }
