a305ed36fef707d3dfd7e1358e0c1126cd4239d3
David Isidore <isidoredavid@protonmail.com>
2023-02-10 15:06:16 +0100

    Affichage des informations sur la page Accomodation

--- src/pages/Accomodation/Accomodation.js
+++ src/pages/Accomodation/Accomodation.js
@@ -14,18 +14,33 @@ export default function Accomodation() {
     console.log(params)
 
     const [appart, showAppart] = useState([])
+    const appartement = datas.filter(data => data.id===params)
 
     useEffect(() => {
         const appartement = datas.filter(data => data.id===params)
-       showAppart(appartement[0].title)
+       showAppart(appartement[0].host.name)
     }, [params])
 
     console.log(appart)
     
+    
     return(
         <div>
             <Header />
-            
+            <div>
+                <div>
+                    <h1>{appartement[0].title} </h1>
+                    <h2>{appartement[0].location}</h2>
+                    <p>{appartement[0].tags}</p>
+                </div>
+                <div>
+                    <div>
+                        <h2>{appartement[0].host.name}</h2>
+                        <img src={appartement[0].host.picture} alt="Visage de l'hôte"/>
+                    </div>
+                    
+                </div>
+            </div>
             <Footer />
         </div>
     )
