3cb8c2e032d2d17924c9418111fc2e3b809f457f
David Isidore <isidoredavid@protonmail.com>
2023-02-10 09:31:26 +0100

    Implémentation de la bannière dans la page About

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
+import Banner from "../../components/Banner/Banner";
 
 export default function About() {
     return(
         <div>
             <Header />
+            <div>
+                <Banner />
+            </div>
             <Footer />
         </div>
     )
