d5763b1671115ffb84f5cc7e533b337eae26cbfd
David Isidore <isidoredavid@protonmail.com>
2023-02-10 09:30:09 +0100

    Implémentation de la bannière dans la page Home

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,5 +1,5 @@
 import React from "react";
-import banner from '../../assets'
+import banner from '../../assets/home_banner.svg'
 
 export default function Banner() {
     return(
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
+import Banner from "../../components/Banner/Banner";
 
 export default function Home() {
     return(
         <div>
             <Header />
+            <div>
+                <Banner />
+            </div>
             <Footer />
         </div>
     )
