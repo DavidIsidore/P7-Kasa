b24cf1d0f1c95c0c6e1c238e589bb50ada9f2f74
David Isidore <isidoredavid@protonmail.com>
2023-02-11 18:52:05 +0100

    Ajout des informations de la page About et import des flèches up et down

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -7,8 +7,22 @@ export default function About() {
     return(
         <div>
             <Header />
+            <Banner />
             <div>
-                <Banner />
+                <div><h2>Fiabilité</h2></div>
+                <p>Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes. </p>
+            </div>
+            <div>
+                 <div><h2>Respect</h2></div>
+                <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme. </p>
+            </div>
+            <div>
+                <div><h2>Services</h2></div>
+                <p>Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.</p>
+            </div>
+            <div>
+                <div><h2>Sécurité</h2></div>
+                <p>La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.</p>
             </div>
             <Footer />
         </div>
