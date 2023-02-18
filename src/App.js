c06373e9e362da344e827459a39bf2795f53c573
David Isidore <isidoredavid@protonmail.com>
2023-02-18 21:45:29 +0100

    Implémentation des routes avec Navigate

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import About from './pages/About/About'
 import NotFound from './pages/NotFound/Notfound'
-
+import { useLocation } from 'react-router-dom'
 import Accomodation from './pages/Accomodation/Accomodation'
+import { Navigate } from 'react-router-dom'
 
-function App() {
+function App({ id }) {
+    const location = useLocation()
+
     return (
         <div className="App">
             <Routes>
@@ -13,6 +16,36 @@ function App() {
                 <Route path="/about" element={<About />} />
                 <Route path="/accomodation/:id" element={<Accomodation />} />
                 <Route path="/*" element={<NotFound />} />
+                <Route
+                    path="/notfound"
+                    element={() =>
+                        (location.pathname = '/' ? (
+                            <Home />
+                        ) : (
+                            <Navigate to="/*" />
+                        ))
+                    }
+                />
+                <Route
+                    path="/notfound"
+                    element={() =>
+                        (location.pathname = '/about' ? (
+                            <About />
+                        ) : (
+                            <Navigate to="/*" />
+                        ))
+                    }
+                />
+                <Route
+                    path="/accomodation/:{id}"
+                    element={() => {
+                        location.pathname = `/accomodation/${id}` ? (
+                            <Accomodation />
+                        ) : (
+                            <Navigate to="/*" />
+                        )
+                    }}
+                />
             </Routes>
         </div>
     )
