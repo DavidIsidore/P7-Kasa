6719260567875b1c6fb9cbd37ba763e2bbe89b10
David Isidore <isidoredavid@protonmail.com>
2023-02-20 09:45:51 +0100

    Replacement du titre des locations

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom'
 import './card.scss'
 
 export default function Card({ id, cover, title }) {
-  return (
-    <Link to={`/accomodation/${id}`} className="singleCard">
-      <img src={cover} alt={title} />
-      <h3>{title}</h3>
-    </Link>
-  )
+    return (
+        <Link to={`/accomodation/${id}`} className="singleCard">
+            <img src={cover} alt={title} />
+            <h3>{title}</h3>
+        </Link>
+    )
 }
